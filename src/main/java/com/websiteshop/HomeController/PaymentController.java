794583b8df94dc519fb385e14b2eafb66aade86c
nguyenducmanh2 <133781963+nguyenducmanh2@users.noreply.github.com>
2024-01-19 17:23:16 +0700

    update

--- src/main/java/com/websiteshop/HomeController/PaymentController.java
+++ src/main/java/com/websiteshop/HomeController/PaymentController.java
@@ -1,9 +1,6 @@
 package com.websiteshop.HomeController;
 
 import com.websiteshop.config.Config;
-import com.websiteshop.model.PaymentResDTO;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
